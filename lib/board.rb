845c17e4105bcb32f64cc55042741e7e6afb917c
Jim Szalewski <jim.szalewski@gmail.com>
2017-06-08 13:37:22 -0600

    create test_helper file

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+require './lib/cell'
+
+class Board
+
+  attr_reader :cells
+
+  def initialize
+    @cells = {}
+  end
+
+  def make_cells
+
+  end
+end
---
+++ test/board_test.rb
@@ -0,0 +1,21 @@
+require './test/test_helper'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_can_create_its_cells
+    board = Board.new
+    cells = board.cells
+    rand_key = 'B2'
+
+    assert_instance_of Hash, cells
+    assert_instance_of Cell, cells[rand_key]
+    assert_equal 16, board.keys.count
+  end
+end
